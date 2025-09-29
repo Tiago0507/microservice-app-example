6bcda2f560c914481a9aee263ec9efa0fda121c0
Tiago0507 <svalenciagarcia707@gmail.com>
2025-09-29 01:07:02 -0500

    fix/ops: changing vm size

--- iac/modules/vm/main.tf
+++ iac/modules/vm/main.tf
@@ -106,7 +106,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "microservices-vm"
   resource_group_name = var.resource_group_name
   location            = var.location
-  size                = "Standard_B4ms"
+  size                = "Standard_B2s"
   admin_username      = var.admin_username
   admin_password      = var.admin_password
   disable_password_authentication = false
